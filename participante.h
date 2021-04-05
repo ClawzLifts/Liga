74c1b2c87100728704ac23ebec2e0532513d7416
Clawz <Pro050197>
2021-04-05 16:18:32 +0200

    update 1

---
+++ participante.h
@@ -0,0 +1,56 @@
+#ifndef LIGA_PARTICIPANTE_H
+#define LIGA_PARTICIPANTE_H
+
+
+typedef struct plantilla{
+    char idUsuario[2];
+    char idPlantilla[3];
+    char nombre[20];
+    int presupuesto;
+    int puntuacion;
+}plantilla;
+
+
+typedef struct jugPlantilla {
+    char idFutbolista[2];
+    char idPlantilla[3];
+}futPlantilla;
+
+
+typedef struct futbolista
+{
+    char idFutbolista[4];
+    char idEquipo[3];
+    char nombre[21];
+    int precio;
+    int puntuacion;
+} futbolista;
+
+
+typedef struct usuario
+{
+    char idUser[2];
+    char nombre[20];
+    char perfil[13];
+    char usuario[5];
+    char password[8];
+}usuario;
+
+
+typedef struct equipo
+{
+    char idEquipo[2];
+    char nombreEquipo[20];
+    int libre;
+}equipo;
+
+
+typedef struct config
+{
+    char data[20];
+    int value;
+
+}config;
+
+
+#endif //LIGA_PARTICIPANTE_H
